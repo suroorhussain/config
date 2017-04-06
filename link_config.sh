23e0553af761d1bff1ea43b0d3ace5d1071c4f60
suroor <surusuroor@gmail.com>
2017-04-06 22:43:10 +0530

    Adds a script to create symlinks for the confs in repo

---
+++ link_config.sh
@@ -0,0 +1,8 @@
+rm ~/.bashrc
+ln -s ~/config/bashrc ~/.bashrc
+rm ~/.bash_aliases
+ln -s ~/config/bash_aliases ~/.bash_aliases
+ln -s ~/config/emacs ~/.emacs
+ln -s ~/config/gitconfig ~/.gitconfig
+ln -s ~/config/pythonrc ~/.pythonrc
+ln -s ~/config/tmux.conf ~/.tmux.conf
