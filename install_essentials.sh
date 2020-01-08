c77a5baaf7878dc0e277c688f99291bed029a472
Suroor Hussain <surusuroor@gmail.com>
2020-01-08 23:00:58 +0530

    chmod +x and add htop

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 apt-get update
-apt-get install git fortune tmux emacs virtualenv virtualenvwrapper vlc gnome-tweak-tool tree
+apt-get install git fortune tmux emacs virtualenv virtualenvwrapper vlc gnome-tweak-tool tree htop
