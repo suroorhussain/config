aaaa3f8cf80e00a220cc9f60005c009f31f6bae1
Suroor Hussain <surusuroor@gmail.com>
2020-01-09 13:08:28 +0530

    Adds python and postgres dev packages to installs

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-get update
-apt-get install git fortune tmux emacs virtualenv virtualenvwrapper vlc gnome-tweak-tool tree htop
+apt-get install git fortune tmux emacs virtualenv virtualenvwrapper vlc gnome-tweak-tool tree htop python3.7-dev libpq-dev
