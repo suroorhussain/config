af3847b9631c7e38a180fdb1b775401c4fde18dc
Suroor Hussain <suroorhussain@hamon.in>
2020-03-18 15:40:27 +0530

    Updates

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get update
-apt-get install git fortune tmux emacs virtualenv virtualenvwrapper vlc gnome-tweak-tool tree htop python3.7-dev libpq-dev
+apt-get update && \
+    apt-get install git fortune tmux emacs virtualenv virtualenvwrapper vlc gnome-tweak-tool tree python 3.7 htop libpq-dev
